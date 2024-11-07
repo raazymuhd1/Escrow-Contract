838cd612b6c5e2b2bc36c63eb11bd82db6e341ac
muhammadraazy <muhammadrazi51@gmail.com>
2024-11-07 08:00:24 -0800

    created a multichain contracts

--- Makefile
+++ Makefile
@@ -1,6 +1,21 @@
+include .env
+
+# Ensure all targets are treated as phony (not representing files)
+.PHONY: install build deploy-sepolia deploy-sepolia-with-args inspect-abi
+
+install all : forge install OpenZeppelin/openzeppelin-contracts --no-commit && forge install forge-std --no-commit
+
+build : forge build
+
+inspect-methods:; forge inspect Escrow methods
+
+deploy-sepolia:; forge script script/DeployEscrow.s.sol:DeployEscrow --rpc-url ${SEPOLIA_RPC_URL} --etherscan-api-key ${ETHERSCAN_API_KEY} --private-key ${PRIVATE_KEY} --legacy --broadcast --verify -vvvv
 
 
-install all ;: forge install OpenZeppelin/openzeppelin-contracts --no-commit && forge install forge-std --no-commit
-
-build;: forge build
-
+sepolia-deploy:; forge create --rpc-url ${SEPOLIA_RPC_URL} \
+    --constructor-args 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
+    --interactive \
+    --etherscan-api-key ${ETHERSCAN_API_KEY} \
+    --verify \
+	--legacy \
+    src/Escrow.sol:Escrow
---
+++ src/Multichain.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IRouterClient} from "@chainlink/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts/src/v0.8/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
+
+
+contract Multichain is CCIPReceiver, OwnerIsCreator {
+    using SafeERC20 for IERC20;
+
+    bytes32 private s_lastReceivedMessageId;
+    string private s_lastReceivedText;
+    IRouterClient private immutable s_router;
+    IERC20 private immutable s_linkToken;
+
+     event MessageReceived(
+        bytes32 indexed messageId, // The unique ID of the message.
+        uint64 indexed sourceChainSelector, // The chain selector of the source chain.
+        address sender, // The address of the sender from the source chain.
+        string text // The text that was received.
+    );
+
+    constructor(address router_, address linkToken) CCIPReceiver(router_) {
+        s_router = IRouterClient(router_);
+        s_linkToken = IERC20(linkToken);
+    }
+
+    // modifier OnlyListedChainAllowed() {
+
+    // }
+
+       /// handle a received message
+    /**
+        @notice implementing a message received 
+     */
+    function _ccipReceive(
+        Client.Any2EVMMessage memory any2EvmMessage
+    ) internal override {
+        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedText = abi.decode(any2EvmMessage.data, (string)); // abi-decoding of the sent text
+
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
+            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
+            abi.decode(any2EvmMessage.data, (string))
+        );
+    }
+
+}
